41136c9b79d925a667af1f4bff32f4cd9d1efe9c
Pramitha Jayasooriya <lpramithamj@gmail.com>
2025-06-18 21:41:41 +0530

    Refactor ranking algorithm for parallel processing; update performance metrics and add new metrics file

--- OpenMP Version/src/ranking.c
+++ OpenMP Version/src/ranking.c
@@ -3,6 +3,7 @@
 #include "../include/utils.h"
 #include "../include/index.h"
 #include "../include/metrics.h"
+#include <omp.h>
 
 // Forward declaration of get_doc_filename
 extern const char* get_doc_filename(int doc_id);
@@ -37,33 +38,57 @@ void rank_bm25(const char *query, int total_docs, int top_k)
     int result_count = 0;
 
     double avg_dl = 0;
-    for (int i = 0; i < total_docs; ++i)
-        avg_dl += get_doc_length(i);
+    
+    // Parallel calculation of average document length
+    #pragma omp parallel
+    {
+        double local_avg_dl = 0;
+        
+        #pragma omp for nowait
+        for (int i = 0; i < total_docs; ++i)
+            local_avg_dl += get_doc_length(i);
+        
+        #pragma omp critical
+        avg_dl += local_avg_dl;
+    }
+    
     avg_dl /= total_docs;
 
+    // Process each query term
     while (token)
     {
         to_lowercase(token);
         if (!is_stopword(token))
         {
             char *term = stem(token);
+            
+            // Find the term in the index
             for (int i = 0; i < index_size; ++i)
             {
                 if (strcmp(index_data[i].term, term) == 0)
                 {
                     int df = index_data[i].posting_count;
                     double idf = log((total_docs - df + 0.5) / (df + 0.5) + 1.0);
-                    for (int j = 0; j < df; ++j)
+                    
+                    // Parallel processing of document scores for the current term
+                    #pragma omp parallel
                     {
-                        int d = index_data[i].postings[j].doc_id;
-                        int tf = index_data[i].postings[j].freq;
-                        double dl = get_doc_length(d);
-                        double score = idf * ((tf * (1.5 + 1)) / (tf + 1.5 * (1 - 0.75 + 0.75 * dl / avg_dl)));
+                        #pragma omp for
+                        for (int j = 0; j < df; ++j)
+                        {
+                            int d = index_data[i].postings[j].doc_id;
+                            int tf = index_data[i].postings[j].freq;
+                            double dl = get_doc_length(d);
+                            double score = idf * ((tf * (1.5 + 1)) / (tf + 1.5 * (1 - 0.75 + 0.75 * dl / avg_dl)));
 
-                        results[d].doc_id = d;
-                        results[d].score += score;
-                        if (d + 1 > result_count)
-                            result_count = d + 1;
+                            #pragma omp critical
+                            {
+                                results[d].doc_id = d;
+                                results[d].score += score;
+                                if (d + 1 > result_count)
+                                    result_count = d + 1;
+                            }
+                        }
                     }
                     break;
                 }
